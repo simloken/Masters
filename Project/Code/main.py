e548c7be6ff3e84feaa97f768b9f58deb3fa4c9a
Morten Hjorth-Jensen <morten.hjorth-jensen@fys.uio.no>
2024-06-03 16:21:16 +0200

    update

--- Project/Code/main.py
+++ Project/Code/main.py
@@ -1,5 +1,6 @@
-from runner import run_neural_network_model, run_restricted_boltzmann_model
-from hamiltonians import RBM, NN
+#from runner import run_neural_network_model, run_restricted_boltzmann_model
+from runner import run_restricted_boltzmann_model
+from hamiltonians import RBM #, NN
 
 # TEMPLATE / HOW TO USE:
     #RBM:
@@ -21,9 +22,9 @@ from hamiltonians import RBM, NN
 
 # run_restricted_boltzmann_model(RBM('two_fermions', 1), 2, 1000, 1000, 3, 1, 2)
 # run_restricted_boltzmann_model(RBM('calogero_sutherland', [1, 2]), 6, 1000, 1000, 33, 1, 1)
-# run_restricted_boltzmann_model(RBM('ising', [-1, -1]), 6, 1000, 1000, 6, 1, 1)
+run_restricted_boltzmann_model(RBM('ising', [-1, -1]), 6, 1000, 1000, 6, 1, 1)
 
 # run_neural_network_model(NN('two_fermions', 1), 2, 5000, 2000, 1, 2)
 # run_neural_network_model(NN('calogero_sutherland', [1, 2]), 6, 5000, 1000, 1, 1)
-run_neural_network_model(NN('ising', [-1, -1]), 6, 5000, 1000, 1, 1)
+#run_neural_network_model(NN('ising', [-1, -1]), 6, 5000, 1000, 1, 1)
 # run_neural_network_model(NN('heisenberg', []), 6, 5000, 1000, 1, 1)
